aad109f91cfacf07186061237b73a5672ffa71c8
Zheng Ren <zheng.ren@accedo.tv>
2021-12-20 02:59:37 +1100

    Initial commit

---
+++ test/hackathon-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HackathonBackend from '../lib/hackathon-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hackathon-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HackathonBackend.HackathonBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
